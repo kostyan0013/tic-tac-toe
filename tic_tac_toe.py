2669fbcb0eb0374668541976aeb25916aeb19c0c
kostyan0013 <kostyan0013@yahoo.com>
2015-01-21 15:33:25 +0500

    tic-tac-toe
    Fixed mistake

--- tic_tac_toe.py
+++ tic_tac_toe.py
@@ -50,11 +50,14 @@ def print_board(board):
                 print 'O',
             elif board[i*3+j] == -1:
                 print i*3+j+1,  
+
             else:
                 print ' ',
+            
             if j != 2:
                 print " | ",
         print
+        
         if i != 2:
             print "-----------------"
         else: 
@@ -72,46 +75,47 @@ def take_side(s):
 def take_corner(s):
     for x in [0,2,6,8]:
         if s[x] == -1: return x
-        
+def take_foreign(s):
+    if (s[0] == 1) or (s[8] == 1):
+        if s[0] == -1: return 0
+        elif s[8] == -1: return 8
+    elif (s[2] == 1) or (s[6] == 1):
+        if s[2] == -1: return 2
+        elif s[6] == -1: return 6
 def bot_turn(s,turn):
     for i in range(3):
         if (fill(s,[i,i+3,i+6],turn) != None): return fill(s,[i,i+3,i+6],turn)
         elif (fill(s,[3*i,3*i+1,3*i+2],turn) != None): return fill(s,[3*i,3*i+1,3*i+2],turn)
     if (fill(s,[0,4,8],turn) != None): return fill(s,[0,4,8],turn)
     elif fill(s,[2,4,6],turn): return fill(s,[2,4,6],turn)
+
     for i in range(3):
         if (fill(s,[i,i+3,i+6],turn+1) != None): return fill(s,[i,i+3,i+6],turn+1)
         elif (fill(s,[3*i,3*i+1,3*i+2],turn+1) != None): return fill(s,[3*i,3*i+1,3*i+2],turn+1)
     if (fill(s,[0,4,8],turn+1) != None): return fill(s,[0,4,8],turn+1)
     elif fill(s,[2,4,6],turn+1): return fill(s,[2,4,6],turn+1)
-    if s[4] == -1: return 4
+    
     if turn % 2 == 1:
-        if (s[0] == 0) and (s[8] == -1): return 8
-        if (s[1] == 0) and ((s[8] == -1) or s[6] == -1):
-            return 6 if s[6] == -1 else 8
-        if (s[2] == 0) and (s[6] == -1):
-            return 6
-        if (s[5] == 0) and ((s[0] == -1) or s[6] == -1):
-            return 0 if s[0] == -1 else 6
-        if (s[8] == 0) and (s[0] == -1): return 0
-        if (s[7] == 0) and ((s[0] == -1) or s[2] == -1):
-            return 0 if s[0] == -1 else 2
-        if (s[6] == 0) and (s[2] == -1): return 2
-        if (s[3] == 0) and ((s[2] == -1) or s[8] == -1):
-            return 2 if s[2] == -1 else 8
+        if turn == 1: return take_corner(s)
+        elif turn == 3: return take_foreign(s)
+        elif s[4] == -1: return 4
+        else: return take_side(s)
+        
     if turn % 2 == 0:
-        if s[4] == 1:
+        if s[4] == -1: return 4
+        elif s[4] == 1:
             if (take_corner(s) != None): return take_corner(s)
             else: return take_side(s)
         elif ((s[0] == 1) or (s[2] == 1) or (s[6] == 1) or (s[8] == 1)):
             if turn == 4:
-                if (take_corner(s) != None): return take_corner(s)
+                if (take_foreign(s) != None): return take_foreign(s)
                 else: return take_side(s)
             else: return take_side(s)
         else:
             if (take_corner(s) != None): return take_corner(s)        
             
     return take_side(s)
+    
     
 def play_bot(mode):
     board = [-1,-1,-1,-1,-1,-1,-1,-1,-1]
@@ -154,7 +158,22 @@ def try_turn(board,turn,current):
         return True
     else:
         print 'Wrong turn'
-        
+    
+def play_gamers():
+    board = [-1,-1,-1,-1,-1,-1,-1,-1,-1]
+    turn = 1
+    while not(check_win(board)):
+        print_board(board)
+        current = raw_input('Choose place for %s' % ('X: ' if turn%2 == 1 else 'O: '))
+        try:
+            current = int(current)-1
+            try_turn(board,turn,current)
+        except:
+            print 'Wrong turn'
+        turn += 1
+    print_board(board)
+    
 while True:
     mode = raw_input("Write 1 to play for X or 0 for O: ")
     play_bot(mode)
+   
